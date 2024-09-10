207a61e79e4aa06135bdc8d5b02b7a3a099c739c
John W Carlson <yottzumm@gmail.com>
2024-09-09 20:02:21 -0500

    Added firefox to IP query (you can only start one firefox though)

--- ip.sh
+++ ip.sh
@@ -2,5 +2,5 @@
 # sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jsonverse
 for container in `sudo docker ps | awk '{ print $1 }' |tail +2`
 do
-	sudo docker exec -it $container hostname -i
+	firefox http://`sudo docker exec -it $container hostname -i`:8088
 done
