fa6166eb672f044d1174076e0a7b06fc61485545
John W Carlson <yottzumm@gmail.com>
2024-09-09 20:04:26 -0500

    Added new window parameter

--- ip.sh
+++ ip.sh
@@ -2,5 +2,5 @@
 # sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jsonverse
 for container in `sudo docker ps | awk '{ print $1 }' |tail +2`
 do
-	firefox http://`sudo docker exec -it $container hostname -i`:8088
+	firefox -new-window http://`sudo docker exec -it $container hostname -i`:8088
 done
